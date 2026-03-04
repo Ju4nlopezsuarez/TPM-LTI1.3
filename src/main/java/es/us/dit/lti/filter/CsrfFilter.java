2c0696f94b8eeea076f15587af964c2bc6643e62
juanlopezsuarez1@gmail.com <juanlopezsuarez1@gmail.com>
2026-03-04 19:44:52 +0100

    Cambio de un error tipográfico

--- src/main/java/es/us/dit/lti/filter/CsrfFilter.java
+++ src/main/java/es/us/dit/lti/filter/CsrfFilter.java
@@ -97,7 +97,7 @@ public class CsrfFilter extends HttpFilter implements Filter {
 					}
 					if (!Objects.equals(launchId, receivedId) && !isMultipart(req)
 							&& !"/tools".equals(req.getServletPath())
-							&& !"oidc_login".equals(req.getServletPath())
+							&& !"/oidc_login".equals(req.getServletPath())
 							&& !"/lti".equals(req.getServletPath())) {
 						// if request is multipart, verification is done in servlet
 						// LTI launch do not use this
