68c0e266bc9353ecde0b25cfd6b28769da320487
juanlopezsuarez1@gmail.com <juanlopezsuarez1@gmail.com>
2026-05-07 13:29:37 +0200

    Cambio de WebServlet

--- src/main/java/es/us/dit/lti/servlet/DeletePlatformServlet.java
+++ src/main/java/es/us/dit/lti/servlet/DeletePlatformServlet.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
 import es.us.dit.lti.entity.Settings;
 import es.us.dit.lti.persistence.LtiPlatformDao;
 
-@WebServlet("/admin/deleteplatform")
+@WebServlet("/admin/DeletePlatformServlet")
 public class DeletePlatformServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DeletePlatformServlet.class);
